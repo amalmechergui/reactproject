01de95a9b7f7e0c007a093fea8f40e944b2689f1
amalmechergui <amal123mechergui@gmail.com>
2023-11-22 19:06:14 +0100

    reactstate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Profile from "./components/Profile";
+import  Timer from "./components/Timer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+
+  constructor () {
+    super();
+    this.state = {
+      fullName: 'Mechergui Amal',
+      bio: 'Born in Aindrahem in 1998',
+      imgSrc: "https://cdn-icons-png.flaticon.com/512/146/146025.png",
+      profession: 'Teacher',
+    show: false,
+  };
+  }
+  showProfile = () => {
+    this.setState({show: !this.state.show});
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.show && (
+      <Profile
+       fullName={this.state.fullName} desc={`${this.state.bio} and I'm a ${this.state.profession}` } 
+      ProfileImage={this.state.imgSrc}/>
+      )}
+
+      <button onClick={this.showProfile}> {this.state.show ? "Hide Profile" : "Show Profile"} </button>
+    <div style={{marginTop: "70px"}}>
+      <Timer />
     </div>
-  );
-}
+  </div>
+    );
+  }
+  }
 
-export default App;
+
+
+
+
+
+export default App ;
